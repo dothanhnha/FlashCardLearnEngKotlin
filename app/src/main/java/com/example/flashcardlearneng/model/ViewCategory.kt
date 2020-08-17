f56b1f7dc686b921b146ee87600da828c9fe257c
dothanhnha <dothanhnha1611@gmail.com>
2020-08-17 15:27:53 +0700

    test push

--- app/src/main/java/com/example/flashcardlearneng/model/ViewCategory.kt
+++ app/src/main/java/com/example/flashcardlearneng/model/ViewCategory.kt
@@ -9,21 +9,35 @@ import com.example.flashcardlearneng.R
 
 class ViewCategory : View {
 
-    val label:String = "People"
+    var label: String
+        set(value) {
+            this.label = value
+        }
+        get() = label
 
-    val amountWord:Int = 0
+    var amountWord: Int
+        set(value) {
+            this.amountWord = value
+        }
+        get() = amountWord
 
-    var percentLearned :Int = 0
+    var percentLearned: Int
+        set(value) {
+        this.percentLearned = value
+    }
+        get() = percentLearned
 
-    val defaultFontSize :Float = 13f
+    val defaultFontSize: Float = 13f
 
-    val offsetTextCircle:Float = 10f
+    val offsetTextCircle: Float = 10f
+    val offsetDetailBottom: Float = 8f
 
     val boundsMaxText = Rect()
 
-    var locationTextPercent : Point = Point()
+    var locationTextPercent: Point = Point()
 
-    var locationLabel : Point = Point()
+    var locationLabel: Point = Point()
+    var locationDetail: Point = Point()
 
     val colorContent = Color.parseColor("#9BFF00")
     val paintContent = Paint()
@@ -35,7 +49,7 @@ class ViewCategory : View {
     val paintRoundLearned = Paint()
 
     val colorContentText = Color.parseColor("#000000")
-    val paintContentText = Paint()
+    val paintLabelText = Paint()
 
     val paintRound = Paint()
 
@@ -43,6 +57,8 @@ class ViewCategory : View {
     val offsetCirclePercent = 6f
 
     var radiusCirclePercent = 0f
+
+    var paintDetailText = Paint()
 
     init {
         paintContent.color = colorContent
@@ -58,7 +74,7 @@ class ViewCategory : View {
 
         paintRound.color = colorContent
         paintRound.style = Paint.Style.STROKE
-        paintRound.strokeWidth = 14f
+        paintRound.strokeWidth = defaultFontSize
         paintRound.isAntiAlias = true
 
         paintRoundLearned.color = colorActive
@@ -66,19 +82,25 @@ class ViewCategory : View {
         paintRoundLearned.strokeWidth = 14f
         paintRoundLearned.isAntiAlias = true
 
-        paintContentText.color = colorContentText
-        paintContentText.style = Paint.Style.FILL
-        paintContentText.isAntiAlias = true
-        paintContentText.setTextSize(defaultFontSize)
+        paintLabelText.color = colorContentText
+        paintLabelText.style = Paint.Style.FILL
+        paintLabelText.isAntiAlias = true
+        paintLabelText.strokeWidth = defaultFontSize
+        paintLabelText.textSize = defaultFontSize
+
+        paintDetailText.color = colorContentText
+        paintDetailText.style = Paint.Style.FILL
+        paintDetailText.isAntiAlias = true
+        paintDetailText.strokeWidth = defaultFontSize
+        paintDetailText.textSize = defaultFontSize
 
     }
 
 
-
-    constructor(context: Context, attrs: AttributeSet): super(context, attrs){
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         val a = context.obtainStyledAttributes(attrs, R.styleable.ViewCategoryStyle)
         if (a.hasValue(R.styleable.ViewCategoryStyle_percentActive)) {
-            this.percentLearned = a.getInteger(R.styleable.ViewCategoryStyle_percentActive,0)
+            this.percentLearned = a.getInteger(R.styleable.ViewCategoryStyle_percentActive, 0)
         }
     }
 
@@ -89,6 +111,7 @@ class ViewCategory : View {
         drawCirclePercent(canvas)
         drawPercent(canvas)
         drawTextContent(canvas)
+        drawTextDetail(canvas)
     }
 
     fun drawContent(canvas: Canvas?) {
@@ -115,7 +138,7 @@ class ViewCategory : View {
         )
     }
 
-    fun drawCirclePercent(canvas: Canvas?){
+    fun drawCirclePercent(canvas: Canvas?) {
         canvas?.drawCircle(
             this.width.toFloat() - this.height / 2.toFloat(),
             this.height / 2.toFloat(),
@@ -125,21 +148,40 @@ class ViewCategory : View {
 
         canvas?.drawArc(
             RectF(
-                this.width - (radiusCirclePercent * 2 + strokeWidth/2 + offsetCirclePercent),
+                this.width - (radiusCirclePercent * 2 + strokeWidth / 2 + offsetCirclePercent),
                 offsetCirclePercent + strokeWidth / 2,
                 this.width - (strokeWidth / 2 + offsetCirclePercent),
                 this.height - (offsetCirclePercent + strokeWidth / 2)
             ),
-            -90f,  (this.percentLearned.toFloat()/100)*360, false, paintRoundLearned
+            -90f, (this.percentLearned.toFloat() / 100) * 360, false, paintRoundLearned
         )
     }
 
-    fun drawPercent(canvas: Canvas?){
-        canvas?.drawText(this.percentLearned.toString()+"%",this.locationTextPercent.x.toFloat(),this.locationTextPercent.y.toFloat(),paintContent)
+    fun drawPercent(canvas: Canvas?) {
+        canvas?.drawText(
+            this.percentLearned.toString() + "%",
+            this.locationTextPercent.x.toFloat(),
+            this.locationTextPercent.y.toFloat(),
+            paintContent
+        )
     }
 
-    fun drawTextContent(canvas: Canvas?){
-        canvas?.drawText(this.label,this.locationLabel.x.toFloat(),this.locationLabel.y.toFloat(),paintContentText)
+    fun drawTextContent(canvas: Canvas?) {
+        canvas?.drawText(
+            this.label,
+            this.locationLabel.x.toFloat(),
+            this.locationLabel.y.toFloat(),
+            paintLabelText
+        )
+    }
+
+    fun drawTextDetail(canvas: Canvas?) {
+        canvas?.drawText(
+            this.amountWord.toString(),
+            this.locationDetail.x.toFloat(),
+            this.locationDetail.y.toFloat(),
+            paintDetailText
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -157,33 +199,65 @@ class ViewCategory : View {
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
 
-        this.radiusCirclePercent = this.height / 2 - ( offsetCirclePercent + strokeWidth / 2)
+        this.radiusCirclePercent = this.height / 2 - (offsetCirclePercent + strokeWidth / 2)
 
-        var scaleFlexText = (this.radiusCirclePercent*2 - this.strokeWidth - this.offsetTextCircle)/this.paintContent.measureText("100%")
+        var scaleFlexText =
+            (this.radiusCirclePercent * 2 - this.strokeWidth - this.offsetTextCircle) / this.paintContent.measureText(
+                "100%"
+            )
 
-        this.paintContent.textSize = this.defaultFontSize*scaleFlexText
+        this.paintContent.textSize = this.defaultFontSize * scaleFlexText
 
 
         val calculatedBoundText = Rect()
 
-        paintContent.getTextBounds(this.percentLearned.toString() + "%",0,this.percentLearned.toString().length+1, calculatedBoundText)
+        paintContent.getTextBounds(
+            this.percentLearned.toString() + "%",
+            0,
+            this.percentLearned.toString().length + 1,
+            calculatedBoundText
+        )
 
         val xCenterCirclePercent = this.width.toFloat() - this.height / 2.toFloat()
-        val yCenterCirclePercent  = this.height / 2.toFloat()
+        val yCenterCirclePercent = this.height / 2.toFloat()
 
-        this.locationTextPercent.x = (xCenterCirclePercent - paintContent.measureText(this.percentLearned.toString() + "%")/2).toInt()
-        this.locationTextPercent.y = (yCenterCirclePercent + calculatedBoundText.height()/2).toInt()
+        this.locationTextPercent.x =
+            (xCenterCirclePercent - paintContent.measureText(this.percentLearned.toString() + "%") / 2).toInt()
+        this.locationTextPercent.y =
+            (yCenterCirclePercent + calculatedBoundText.height() / 2).toInt()
 
         /////////////////////////
 
+        val boundTextDefault = Rect()
 
+        paintLabelText.getTextBounds(
+            this.label,
+            0,
+            this.label.toString().length,
+            boundTextDefault
+        )
 
-        var scaleFlexLabel = (this.width - 3*this.height/2 - this.offsetTextCircle)/this.paintContentText.measureText(this.label)
+        var scaleFlexLabel = ((2f / 6f) * (this.height) / boundTextDefault.height())
 
-        this.paintContentText.textSize = this.defaultFontSize*scaleFlexLabel
+        this.paintLabelText.textSize = this.defaultFontSize * scaleFlexLabel
 
-        paintContentText.getTextBounds(this.label,0,this.label.toString().length, calculatedBoundText)
-        locationLabel.x = height/2
-        locationLabel.y = height/2 + calculatedBoundText.height()/2
+        paintLabelText.getTextBounds(this.label, 0, this.label.length, calculatedBoundText)
+        locationLabel.x = height / 2
+        locationLabel.y = height / 2 + calculatedBoundText.height() / 2
+
+        ////////////////////
+
+        this.paintDetailText.textSize = 1f / 2f * this.paintLabelText.textSize
+
+        paintDetailText.getTextBounds(
+            this.amountWord.toString(),
+            0,
+            this.amountWord.toString().length,
+            calculatedBoundText
+        )
+        locationDetail.x =
+            this.width - this.height - paintDetailText.measureText(this.amountWord.toString())
+                .toInt()
+        locationDetail.y = height - offsetDetailBottom.toInt()
     }
 }
